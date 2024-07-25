c0424e0b2756becaccf0f61801c1956b2488a845
ancora000 <115628833+ancora000@users.noreply.github.com>
2024-07-26 07:42:39 +0900

    object private styling

--- js/app.d3103a3b.js
+++ js/app.d3103a3b.js
@@ -7708,6 +7708,7 @@
                 },
                 computed: {
                     styling: function() {
+						if ("private" === this.from) if("undefined" === typeof this.row.styling) this.$set(this.row, "styling", {});
                         return "private" === this.from ? this.row.styling : this.$store.state.app.styling
                     },
 					filterStyling: function() {
@@ -17238,6 +17239,7 @@
 									if (e.app.rows[b].objects[c].isPrivateStyling && "undefined" === typeof e.app.rows[b].objects[c].privateObjectImageIsOn) e.$set(e.app.rows[b].objects[c], "privateObjectImageIsOn", !0);
 									if (e.app.rows[b].objects[c].isPrivateStyling && "undefined" === typeof e.app.rows[b].objects[c].privateObjectIsOn) e.$set(e.app.rows[b].objects[c], "privateObjectIsOn", !0);
 									if (e.app.rows[b].objects[c].isPrivateStyling && "undefined" === typeof e.app.rows[b].objects[c].privateBackgroundIsOn) e.$set(e.app.rows[b].objects[c], "privateBackgroundIsOn", !0);
+									if (e.app.rows[b].objects[c].isPrivateStyling && "undefined" === typeof e.app.rows[b].objects[c].styling) e.$set(e.app.rows[b].objects[c], "styling", {});
 									if (e.app.rows[b].objects[c].isMultipleUseVariable && "number" !== typeof e.app.rows[b].objects[c].initMultipleTimesMinus) {
 										e.$set(e.app.rows[b].objects[c], "initMultipleTimesMinus", 0);
 										e.$set(e.app.rows[b].objects[c], "initMultipleTimesMinus", parseInt(e.app.rows[b].objects[c].numMultipleTimesMinus));
