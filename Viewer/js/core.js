388177ca3fc73a98a870afaa44d4dc11f2e64f81
wahaha303 <115628833+wahaha303@users.noreply.github.com>
2025-10-05 18:52:09 +0900

    v2.6.0

--- Viewer/js/core.js
+++ Viewer/js/core.js
@@ -24,9 +24,9 @@ add('link', { rel: 'stylesheet', href: basePath + 'css/bootstrap.min.css' });
 add('link', { rel: 'stylesheet', href: basePath + 'css/material-icons.css' });
 add('script', { src: basePath + 'js/polyfills.js' });
 add('link', { rel: 'stylesheet', href: basePath + 'css/chunk-vendors.D62pGNex.css', crossorigin: '' });
-add('link', { rel: 'stylesheet', href: basePath + 'css/app.DsGj202b.css', crossorigin: '' });
-add('link', { rel: 'modulepreload', href: basePath + 'js/chunk-vendors.BkzMju_X.js', crossorigin: '' });
-add('script', { type: 'module', src: basePath + 'js/app.BcaDlQXM.js', crossorigin: '' });
+add('link', { rel: 'stylesheet', href: basePath + 'css/app.iLkCVkUh.css', crossorigin: '' });
+add('link', { rel: 'modulepreload', href: basePath + 'js/chunk-vendors.CBq834O2.js', crossorigin: '' });
+add('script', { type: 'module', src: basePath + 'js/app.DiQszWKx.js', crossorigin: '' });
 add('script', { type: 'module', text: 'import.meta.url;import("_").catch(() => 1);(async function*(){})().next();window.__vite_is_modern_browser = true;' });
 add('script', { type: 'module', text: '!function(){if(window.__vite_is_modern_browser) return;console.warn("vite: loading legacy chunks, syntax error above and the same error below should be ignored");var e = document.getElementById("vite-legacy-polyfill");var n = document.createElement("script");n.src = e.src;n.onload = function(){System.import(document.getElementById("vite-legacy-entry").getAttribute("data-src"));};document.body.appendChild(n);}();' });
 addEventListener('DOMContentLoaded', () => {
@@ -41,7 +41,7 @@ addEventListener('DOMContentLoaded', () => {
     polyfillScript.id = 'vite-legacy-polyfill';
     polyfillScript.src = basePath + 'js/app-legacy.wnBmmXsE.js';
     polyfillScript.onload = function(){
-        var entrySrc = basePath + 'js/app-legacy.BAd-_ci5.js';
+        var entrySrc = basePath + 'js/app-legacy.JW8W1nnl.js';
         function tryImport(retries) {
             if (window.System && typeof window.System.import === 'function') {
                 window.System.import(entrySrc);
