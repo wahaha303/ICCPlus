feb6768e1571fcaf3a0f2cf9ad9f3d7445567780
wahaha303 <115628833+wahaha303@users.noreply.github.com>
2026-02-19 23:14:44 +0900

    v2.8.5

--- Viewer/js/core.js
+++ Viewer/js/core.js
@@ -26,7 +26,7 @@ add('script', { src: basePath + 'js/polyfills.js' });
 add('link', { rel: 'stylesheet', href: basePath + 'css/chunk-vendors.D62pGNex.css', crossorigin: '' });
 add('link', { rel: 'stylesheet', href: basePath + 'css/app.iLkCVkUh.css', crossorigin: '' });
 add('link', { rel: 'modulepreload', href: basePath + 'js/chunk-vendors.D6_oVHyc.js', crossorigin: '' });
-add('script', { type: 'module', src: basePath + 'js/app.B7rUuxRq.js', crossorigin: '' });
+add('script', { type: 'module', src: basePath + 'js/app.B-pNIR2k.js', crossorigin: '' });
 add('script', { type: 'module', text: 'import.meta.url;import("_").catch(() => 1);(async function*(){})().next();window.__vite_is_modern_browser = true;' });
 add('script', { type: 'module', text: '!function(){if(window.__vite_is_modern_browser) return;console.warn("vite: loading legacy chunks, syntax error above and the same error below should be ignored");var e = document.getElementById("vite-legacy-polyfill");var n = document.createElement("script");n.src = e.src;n.onload = function(){System.import(document.getElementById("vite-legacy-entry").getAttribute("data-src"));};document.body.appendChild(n);}();' });
 addEventListener('DOMContentLoaded', () => {
@@ -41,7 +41,7 @@ addEventListener('DOMContentLoaded', () => {
     polyfillScript.id = 'vite-legacy-polyfill';
     polyfillScript.src = basePath + 'js/app-legacy.D5yyI6pR.js';
     polyfillScript.onload = function(){
-        var entrySrc = basePath + 'js/app-legacy.DPCJ4wZA.js';
+        var entrySrc = basePath + 'js/app-legacy.DHJE6lnG.js';
         function tryImport(retries) {
             if (window.System && typeof window.System.import === 'function') {
                 window.System.import(entrySrc);
