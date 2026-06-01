1dd8f9c717a8cf236426cf05280d798bb1ac3c57
wahaha303 <115628833+wahaha303@users.noreply.github.com>
2026-06-01 18:57:42 +0900

    v2.9.14

--- Viewer/js/core.js
+++ Viewer/js/core.js
@@ -24,9 +24,9 @@ add('link', { rel: 'stylesheet', href: basePath + 'css/bootstrap.min.css' });
 add('link', { rel: 'stylesheet', href: basePath + 'css/material-icons.css' });
 add('script', { src: basePath + 'js/polyfills.js' });
 add('link', { rel: 'stylesheet', href: basePath + 'css/chunk-vendors.D62pGNex.css', crossorigin: '' });
-add('link', { rel: 'stylesheet', href: basePath + 'css/app.D-kb0RUj.css', crossorigin: '' });
+add('link', { rel: 'stylesheet', href: basePath + 'css/app.yD6yS01H.css', crossorigin: '' });
 add('link', { rel: 'modulepreload', href: basePath + 'js/chunk-vendors.BlIj3Rbr.js', crossorigin: '' });
-add('script', { type: 'module', src: basePath + 'js/app.DolPiax4.js', crossorigin: '' });
+add('script', { type: 'module', src: basePath + 'js/app._lzkjJma.js', crossorigin: '' });
 add('script', { type: 'module', text: 'import.meta.url;import("_").catch(() => 1);(async function*(){})().next();window.__vite_is_modern_browser = true;' });
 add('script', { type: 'module', text: '!function(){if(window.__vite_is_modern_browser) return;console.warn("vite: loading legacy chunks, syntax error above and the same error below should be ignored");var e = document.getElementById("vite-legacy-polyfill");var n = document.createElement("script");n.src = e.src;n.onload = function(){System.import(document.getElementById("vite-legacy-entry").getAttribute("data-src"));};document.body.appendChild(n);}();' });
 addEventListener('DOMContentLoaded', () => {
@@ -41,7 +41,7 @@ addEventListener('DOMContentLoaded', () => {
     polyfillScript.id = 'vite-legacy-polyfill';
     polyfillScript.src = basePath + 'js/app-legacy.D5yyI6pR.js';
     polyfillScript.onload = function(){
-        var entrySrc = basePath + 'js/app-legacy.BwnP1ekS.js';
+        var entrySrc = basePath + 'js/app-legacy.D7NiMxA8.js';
         function tryImport(retries) {
             if (window.System && typeof window.System.import === 'function') {
                 window.System.import(entrySrc);
